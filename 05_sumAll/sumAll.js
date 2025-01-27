80456247deb28004166743897d1d48a1aae51b78
Candlewind <193207554+Candlewinds@users.noreply.github.com>
2025-01-26 17:41:38 -0800

    End of afternoon 1-26-2025

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
+let x = 5;
+let y = 37;
 
+const sumAll = function (x, y) {
+    if (x < 0 || y < 0) {
+        return "ERROR";
+    }
+    if (!Number.isInteger(x) || !Number.isInteger(y)) {
+        return "ERROR";
+    }
+
+    [seqStart, seqEnd] = [Math.min(x, y), Math.max(x, y)];
+    return (seqEnd * (seqEnd + 1)) / 2 - ((seqStart - 1) * seqStart) / 2;
 };
+
+console.log(sumAll(x, y));
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (F) {
+    return Number(((5 / 9) * (F - 32)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (C) {
+    return Number((C * (9 / 5) + 32).toFixed(1));
 };
+
+console.log(convertToCelsius(41));
+console.log(convertToFahrenheit(41));
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit,
 };
